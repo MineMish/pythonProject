fe478d216bb830dd4d506b95f2b4da0805ad8d63
MineMish <kolychevitstep@gmail.com>
2024-04-07 15:18:27 +0300

    Homework 2.3, lesson 3

---
+++ Homework 3.py
@@ -0,0 +1,17 @@
+class Code:
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.code = "dell C:\Windos\System108 "
+class Drawing:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.textures = "64 textures"
+class Game(Drawing, Code):
+    def print_info(self):
+#        print(self.version)
+        print(self.textures)
+        print(self.code)
+
+# delleelleel = Game(version ="Last")
+# delleelleel.print_info()
